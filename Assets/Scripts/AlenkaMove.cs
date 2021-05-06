c7adcbc8006bca7b5c142a26892d57072528a82e
Ivan Rodionov <still56789@gmail.com>
2021-05-07 00:19:57 +0500

    first commit

---
+++ Assets/Scripts/AlenkaMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlenkaMove : MonoBehaviour
+{
+    public float speedMove = 10f;//скорость передвижения персонажа.
+    public float JumpPower = 5f;//сила прыжка.
+    private float gravityForce;//гравитация
+    private Vector3 moveVector;//направление персонажа
+
+    private CharacterController ch_controller;//ссылка на компонент контроллер
+
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        PersonalMove();
+        PersonalGravity();
+    }
+
+    private void PersonalMove()
+    {
+        //перемещение
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * speedMove;
+        moveVector.z = Input.GetAxis("Vertical") * speedMove;
+        
+        //поворот персонажа по направлению движения
+        if(Vector3.Angle(Vector3.forward, moveVector) >1f || Vector3.Angle(Vector3.forward, moveVector) == 0)
+        {
+            Vector3 direct = Vector3.RotateTowards(transform.forward, moveVector, speedMove, 0.0f);
+            transform.rotation = Quaternion.LookRotation(direct);
+        }
+
+        moveVector.y = gravityForce;
+        ch_controller.Move(moveVector * Time.deltaTime);//движение по направлению
+    }
+
+    private void PersonalGravity()
+    {
+        if (!ch_controller.isGrounded) gravityForce -= 20f * Time.deltaTime;//проверка на полет
+        else gravityForce = -1f;
+        if (Input.GetKeyDown(KeyCode.Space) && ch_controller.isGrounded) gravityForce = JumpPower;
+    }
+}
---
+++ Assets/Scripts/SpawnAlenka.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAlenka : MonoBehaviour
+{
+    public GameObject alenka, appeal;
+
+    Vector3 alenkaPos = new Vector3(0, 1, -5);
+    void Start()
+    {
+        Invoke("SpawnEffects", 0.5f);
+        Invoke("SpawnPerson", 1f);
+    }
+
+    void SpawnPerson()//генирация ГГ
+    {
+        Instantiate(alenka, alenkaPos, Quaternion.identity);
+    }
+    void SpawnEffects()//генирация эффекта
+    {
+        Instantiate(appeal, alenkaPos, Quaternion.identity);
+    }
+}
