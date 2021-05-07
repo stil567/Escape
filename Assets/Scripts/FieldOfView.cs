9ce5a195cb7db82fdbf0eff4a35d10bf796d4e27
Ivan Rodionov <still56789@gmail.com>
2021-05-07 22:30:02 +0500

    добавлена механика ловли героя врагом

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+
+    public GameObject alenka;//объект поиска
+
+    //слои для препятствий, объектов
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeeAlenka;//проверка на видимость персонажа
+
+    private void Start()
+    {
+        alenka = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine() //проверка на видимость с задержкой
+    {
+
+        WaitForSeconds wait = new WaitForSeconds(0.2f);  //фактическая задержка в процедуре 5 раз в секунду
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeCheck = Physics.OverlapSphere(transform.position, radius, targetMask); //центральная позиция,радиус, маска
+
+        if (rangeCheck.Length != 0)
+        {
+            Transform target = rangeCheck[0].transform; //первый из массива, т.к. один игрок
+            Vector3 directionToTarget = (target.position - transform.position).normalized; //разница между позицией игрока и врага, получаем направление
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2) //проверка на появление в поле зрения врага персонажа 
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position); //находим расстояние между врагов и персонажем для ограничения луча
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)) //если не наткнулись на препятствие, то персонаж замечен
+                    canSeeAlenka = true;
+                else
+                    canSeeAlenka = false;
+            }
+            else
+                canSeeAlenka = false;
+        }
+        else if (canSeeAlenka) //если раньше были в поле зрении врага
+            canSeeAlenka = false;
+    }
+
+
+}
--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 0.5f;//скорость перемещения патруля
 
-    public Transform[] moveSpots;
-    private int randomSpot;
-    private float waitTime;
-    public float startWaitTime;
+    public Transform[] moveSpots;//точки которые патрулирует персонаж
+    private int randomSpot;//выбраная слуяайно точка для патруля
+    private float waitTime;//счетчик ожидания
+    public float startWaitTime;//количество секунд ожидания персонажа в точке патруля
 
     void Start()
     {
@@ -19,6 +19,10 @@ public class Patrol : MonoBehaviour
 
     void Update()
     {
+        Vector3 direction = moveSpots[randomSpot].transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+
         transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
         
         if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
